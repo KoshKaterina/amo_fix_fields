aa19628680d093e0baeb2311df1bc83123cf9b59
l1ghtny <ankoshenkov@gmail.com>
2025-12-14 20:22:48 +0000

    added better logging

--- memory.py
+++ memory.py
@@ -4,13 +4,14 @@ from datetime import datetime
 
 from api import add_info_from_ms
 
-logger = logging.getLogger('unicorn')
+logger = logging.getLogger("uvicorn")
 
 
 async def update_info_later(goods, delivery_type, delivery_address, lead_id, execute_after, lead_last_processed):
-
-    if execute_after > 0:
+    try:
         await asyncio.sleep(execute_after)
-    logger.info(f"Patching scheduled changes in {lead_id} now...")
-    await add_info_from_ms(goods=goods, delivery_type=delivery_type, delivery_address=delivery_address, lead_id=lead_id)
-    lead_last_processed[lead_id] = datetime.now()
+        logger.info(f"Patching scheduled changes in {lead_id} now...")
+        await add_info_from_ms(goods=goods, delivery_type=delivery_type, delivery_address=delivery_address, lead_id=lead_id)
+        lead_last_processed[lead_id] = datetime.now()
+    except Exception as e:
+        logger.error(e)
